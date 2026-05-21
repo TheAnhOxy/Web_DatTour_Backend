a79f9f990f9cf5c612be5dd025ba9403e755b7a5
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-05-22 00:11:35 +0700

    refactor: fix booking contact for user

--- Booking-service/src/main/java/com/tour/booking/dto/request/BookingRequest.java
+++ Booking-service/src/main/java/com/tour/booking/dto/request/BookingRequest.java
@@ -1,6 +1,11 @@
 package com.tour.booking.dto.request;
 
 import com.tour.booking.dto.PassengerDTO;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +18,22 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class BookingRequest {
+
+
     private Long userId;
     private Long departureId;
     private List<PassengerDTO> passengers;
     private String note;
+
+    @NotBlank(message = "Tên người liên hệ không được để trống")
+    @Size(max = 100, message = "Tên liên hệ không được vượt quá 100 ký tự")
     private String contactName;
+    @NotBlank(message = "Email liên hệ không được để trống")
+    @Email(message = "Định dạng Email không hợp lệ")
+    @Size(max = 100, message = "Email không được vượt quá 100 ký tự")
     private String contactEmail;
+
+    @NotBlank(message = "Số điện thoại không được để trống")
+//    @Pattern(regexp = "^(0|\\+84)[3|5|7|8|9][0-9]{8}$", message = "Số điện thoại không đúng định dạng Việt Nam")
     private String contactPhone;
 }
